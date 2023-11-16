2f565673c023f29f8539887bff7c056de0a796b7
Akif <mdakif@ualberta.ca>
2023-11-16 13:02:43 -0700

    Added utilities for randomly selecting AST components and parameters from scope. Also, added functions to generate vectorizable statements and loop.

---
+++ src/GenVectorizable.hs
@@ -0,0 +1,62 @@
+module GenVectorizable where
+
+import Control.Monad
+import Control.Monad.Trans.State
+import Language.C.Data.Ident
+import Language.C.Data.Node (undefNode)
+import Language.C.Data.Position (nopos)
+import Language.C.Syntax.AST
+import Common
+import CommonGen
+import Selectors
+
+{- 
+Goal:
+  - Create a loops with arbitrary index variables.
+  - Use modulo to keep access within bounds.
+-}
+
+-- (1) Loops with no nested control statements
+genVectorizableForForward :: GState CStat
+genVectorizableForForward = do
+  -- 1. Activate an index variable
+  (key, activeIndex) <- activateIndexVar
+  -- 2. TODO: Create a vectorizable loop body
+  --    -- Since the state is updated with the index variable we can create the loop body
+  body <- genVectorizableBlock
+  -- 3. Create the for loop
+  let 
+    indexExpr :: CExpr = CVar (activeIndexIdent activeIndex) undefNode
+    initExpr :: CExpr = CAssign CAssignOp indexExpr (constructExprFromEither $ activeIndexStart activeIndex) undefNode
+    conditionExpr :: CExpr = CBinary CLeOp indexExpr (constructExprFromEither $ activeIndexEnd activeIndex) undefNode
+    updateExpr :: CExpr = CAssign CAddAssOp indexExpr (constructExprFromEither $ activeIndexStride activeIndex) undefNode
+    loopStat = 
+      CFor 
+        (Left $ Just initExpr)
+        (Just conditionExpr)
+        (Just updateExpr)
+        body
+        undefNode
+  -- 4. Create a label that will be replaced with the pragma call
+  stat <- constructPragmaLabel loopStat
+  -- 6. Deactivate Index var
+  deactiveIndexVar key
+  pure stat
+
+constructPragmaLabel :: CStat -> GState CStat
+constructPragmaLabel targetStat = do
+  cNameId <- getId
+  let name = "pragma"
+      ident = mkIdent nopos name cNameId
+  pure $ CLabel ident targetStat [] undefNode
+
+
+{-
+    Generates a block of assignments
+-}
+genVectorizableBlock :: GState CStat
+genVectorizableBlock = do
+    noOfStats <- execRandGen (1, 5) -- TODO: Change hard-coded constant
+    ((flip (CCompound []) undefNode . fmap CBlockStmt)  <$>) . replicateM noOfStats $ do
+        dtype <- gets targetDTypes >>= chooseFromList
+        flip CExpr undefNode . Just <$> genAssignExpr dtype
---
+++ src/Selectors.hs
@@ -0,0 +1,77 @@
+module Selectors where
+
+import Common
+import Control.Monad.Trans.State
+import Data.IntMap qualified as IntMap
+import Data.Vector qualified as V
+import Language.C.Data.Ident
+
+{-
+   Activates an index variable from the IndexVars list
+    - Activates = assigns (start, end, stride)
+    - The values of the parameter is either:
+        - (1) Integer Literal (TODO)
+              - start: [0, 10] -- Can be Uniform
+              - end: [10, 10_000] -- TODO: Would want to use a skewed-left RV
+              - stride: [1, 5] -- Can be Uniform
+        - (2) Scalar Identifier (TODO)
+              - start: [Index, ?]
+              - end: [dimension sizes]
+              - stride: [Index, ?]
+-}
+activateIndexVar :: GState (Int, ActiveIndexVar)
+activateIndexVar = do
+    (key, ident) <- gets indexVars >>= chooseKeyFromMap
+    -- Remove index from index variable list
+    modify' (\s -> s { indexVars = IntMap.delete key (indexVars s) } )
+    -- TODO: Currently just doing (1)
+    startVal <- execRandGen (0, 10)
+    endVal <- execRandGen (10, 10_000)
+    strideVal <- execRandGen (1, 5)
+    let activeIndexVar = ActiveIndexVar ident (Left startVal) (Left endVal) (Left strideVal)
+    -- Add it to the active index variable list
+    modify' (\s -> s { activeIndexes = IntMap.insert key activeIndexVar (activeIndexes s) } )
+    pure (key, activeIndexVar)
+    
+
+deactiveIndexVar :: Int -> GState ()
+deactiveIndexVar key = do
+    ident <- gets (activeIndexIdent . (IntMap.! key) . activeIndexes)
+    -- Remove the active index variable from the active list
+    modify' (\s -> s { activeIndexes = IntMap.delete key (activeIndexes s) } )
+    -- Add it back to the index variable list
+    updateIndexes key ident
+    
+
+chooseKeyFromMap :: IntMap.IntMap a -> GState (Int, a)
+chooseKeyFromMap m = do
+    let keys :: V.Vector Int = V.fromList $ IntMap.keys m
+    i <- execRandGen (0, V.length keys - 1)
+    let k = keys V.! i
+    pure (k, m IntMap.! k)
+
+chooseSingleton :: DType -> GState Ident
+chooseSingleton dtype = do
+    (_, ident) <- gets ((V.! fromEnum dtype) . singletons) >>= chooseKeyFromMap
+    pure ident
+
+chooseArray :: DType -> GState ArrSpec
+chooseArray dtype = do
+    (_, arrSpec) <- gets ((V.! fromEnum dtype) . mDimArrs) >>= chooseKeyFromMap
+    pure arrSpec
+
+chooseActiveIndex :: GState ActiveIndexVar
+chooseActiveIndex = do
+    (_, activeIndexVar) <- gets activeIndexes >>= chooseKeyFromMap
+    pure activeIndexVar
+
+chooseFromList :: [a] -> GState a
+chooseFromList xs = (xs !!) <$> execRandGen(0, length xs - 1) -- TODO: distribution
+
+
+-- chooseFromVMap :: (SProg -> Map.Map k (a,b)) -> GState a
+-- chooseFromVMap f = do
+--   mp <- f <$> get
+--   let n = Map.size mp
+--   i <- execRandGen (0, n-1)
+--   pure . fst . snd $ Map.elemAt i mp
